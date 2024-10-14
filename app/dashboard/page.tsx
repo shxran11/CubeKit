2c946ff9c4fcc4f2b879ba81364465df69087510
shxran11 <saranya.podder.fiem.cse21.@teamfuture.in>
2024-10-14 20:19:26 +0530

    Implemented User Authentication

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <UserButton />
+    </div>
+  );
+};
+
+export default DashboardPage;
