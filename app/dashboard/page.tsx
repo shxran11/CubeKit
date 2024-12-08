1c2a489eeea87c4df1d3b753ce58a424c0440533
shxran11 <saranya.podder.fiem.cse21.@teamfuture.in>
2024-12-08 12:02:20 +0530

    small change in UI

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -14,7 +14,7 @@ const DashboardPage = () => {
     <>
       <div className="flex flex-col md:flex-row justify-between items-start md:items-center gap-5 md:gap-0">
         <div>
-          <p className="text-3xl font-bold">Hello, {user.fullName}</p>
+          <p className="text-3xl font-bold">Hello, {user.firstName}</p>
           <p className="text-sm text-gray-500">
             Create smart AI generated courses, instantly.
           </p>
